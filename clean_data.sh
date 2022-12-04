5a8f72abc20b3da4b24cceb4d5c040032f11c49a
Jiri Dohnalek <jiri@groundwow.com>
2022-12-04 19:34:04 +0000

    Update clean_data.sh

--- clean_data.sh
+++ clean_data.sh
@@ -1,5 +1,7 @@
 sudo rm -rf ./deploy/mysql/data/*
-touch ./deploy/mysql/data/.gitkeep
+touch ./deploy/mysql/data/.gitignore
+echo "!.gitignore" > ./deploy/mysql/data/.gitignore 
 
 sudo rm -rf ./deploy/redis/data/*
-touch ./deploy/redis/data/.gitkeep
+touch ./deploy/redis/data/.gitignore
+echo "!.gitignore" > ./deploy/mysql/redis/.gitignore 
